3c780f1a7d44cd21ab6049b270d8eb661be73ef9
Piyush1002 <piyusharma166@gmail.com>
2021-08-09 18:22:37 +0530

    last commit today

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -702,4 +702,26 @@ $(function () {
       },
     },
   });
+
+  $(".gems-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    autoplay: true,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true,
+      },
+      600: {
+        items: 1,
+        nav: false,
+      },
+      1000: {
+        items: 4,
+        nav: false,
+        loop: true,
+      },
+    },
+  });
 });
